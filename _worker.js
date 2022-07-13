da67e4dc75b8d3790ce5bd838a0d01294372687b
tawayaka <30314539+tawayaka@users.noreply.github.com>
2022-07-13 08:30:39 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'google.com/404'
+      url.hostname = 'https://docker-tachidesk-production.up.railway.app'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
