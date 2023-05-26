2cd781e2051d5c5a13725973dc6137bab44c6f2f
tawayaka <30314539+tawayaka@users.noreply.github.com>
2023-05-26 17:11:57 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'docker-tachide-prod-xka-ahbj8p.mo2.mogenius.io:80'
+      url.hostname = 'api.pawan.krd'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
