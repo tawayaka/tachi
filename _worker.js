da6c491f1e5257a2abf413560533fba7af55cdce
tawayaka <30314539+tawayaka@users.noreply.github.com>
2024-04-08 17:42:18 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'voz.vn';
+      url.hostname = 'google.com';
       let new_request = new Request(url, request);
       
       // Check if the request contains a cookie
